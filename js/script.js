a61a91b30a315caa4f9fb17bab4714292c324a2f
ffozilbek <fozilbekfayzullayev05@gmail.com>
2024-07-08 06:34:39 +0500

    add hero info buttons, js counter

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ const elBannerImg = document.querySelector(".banner__img"),
     elLightBoxImg = document.querySelector(".light-box__img"),
     elsThumnailImgWrapper = document.querySelectorAll(".banner-bottom__img-wrapper"),
     elsThumnailImg = document.querySelectorAll(".banner-bottom__img"),
+    elsThumnailLightBoxImg = document.querySelectorAll(".banner-bottom__lightbox-img"),
     elLightBox = document.querySelector(".light-box"),
+    elsLightBoxThumnailImgs = document.querySelectorAll(".banner-bottom__lightbox-img-wrapper"),
     elLightBoxClose = document.querySelector(".light-box__close"),
     elLightBoxOpen = document.querySelector(".hero__banner-img-wrapper"),
     elHeaderTrash = document.querySelector(".header__trash"),
@@ -15,11 +17,24 @@ const elBannerImg = document.querySelector(".banner__img"),
             el.addEventListener("click", ()=> {
                 elBannerImg.setAttribute("src",elThumnailImg.dataset.imgSrc);
                 elBannerImg.setAttribute("srcset",elThumnailImg.dataset.imgSrcBig);
-                elLightBoxImg.setAttribute("src",elThumnailImg.dataset.imgSrc);
-                elLightBoxImg.setAttribute("srcset",elThumnailImg.dataset.imgSrcBig);
 
                 el.classList.add("active");
                 img.parentElement.classList.remove("active");
+            })
+        })
+    })
+
+    elsThumnailLightBoxImg.forEach(image => {
+        // LIGHT BOX
+        elsLightBoxThumnailImgs.forEach(elImg=> {
+            const elLightBoxThumnailImg = elImg.querySelector(".banner-bottom__img");
+
+            elImg.addEventListener("click", ()=> {
+                elLightBoxImg.setAttribute("src",elLightBoxThumnailImg.dataset.imgSrc);
+                elLightBoxImg.setAttribute("srcset",elLightBoxThumnailImg.dataset.imgSrcBig);
+
+                elImg.classList.add("lightbox-thumnail-active");
+                image.parentElement.classList.remove("lightbox-thumnail-active");
             })
         })
     })
@@ -41,3 +56,28 @@ const elBannerImg = document.querySelector(".banner__img"),
             elShoppingCard.classList.toggle("shopping-cart__open");
         })
     }
+
+    // COUNTER
+    const elButtonMinus = document.querySelector(".button-minus"),
+        elButtonPlus = document.querySelector(".button-plus"),
+        elShoppingQuantity = document.querySelector(".header__product-quantity");
+    let elCounterValue = document.querySelector(".counter");
+    let count = 0;
+
+        if(elButtonPlus) {
+            elButtonPlus.addEventListener("click", ()=> {
+                count++;
+                elCounterValue.textContent = count;
+                elShoppingQuantity.textContent = count;
+            })
+        }
+
+        if(elButtonMinus) {
+            elButtonMinus.addEventListener("click", ()=> {
+                if(count!==0) {
+                    count--;
+                    elCounterValue.textContent = count;
+                elShoppingQuantity.textContent = count;
+                }
+            })
+        }
